37f1c962908a4fd64187161030147be7c8f1b411
TedwardReed <100960539+TedwardReed@users.noreply.github.com>
2025-08-07 13:07:32 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
-export const leagueID = "1201891419585785856"; // your league ID
-export const leagueName = "The Legends League"; // your league name
+export const leagueID = "1257829197557006336"; // your league ID
+export const leagueName = "The Independence League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
